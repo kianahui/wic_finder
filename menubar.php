d66b259e5e35f06bf4bca295c590e3a8eaec01c8
lilysierra1 <lilysierra1@gmail.com>
2013-10-21 14:01:34 -0700

    Created menubar.php

---
+++ menubar.php
@@ -0,0 +1,22 @@
+<!--This code is for the menu bar on the right that will display the logo and the 5 user options-->
+<html>
+	<body>
+		<div id="sidebar">
+            <ul>
+                <li><img src="http://www.sourcetech.com/uploads/cms/image/wic_logo.jpg"/>
+                <li><a href="#">Find a clinic</a></li>
+                <li><a href="#">Find a WIC Stores</a></li>
+                <li><a href="#">Find a WIC Farmer's market</a></li>
+                <li><a href="#">See Food List</a></li>
+                <li><a href="#">Find a recipe</a></li>
+             </ul>
+        </div>
+        <div class="main-content">
+        	<a href="#" data-toggle=".container" id="sidebar-toggle">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a> 
+      	 </div>
+    </body>
+</html>
