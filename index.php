06a1ea90e808747740b414b8b1c89a807c27f896
lilysierra1 <lilysierra1@gmail.com>
2013-10-25 14:57:50 -0700

    Update index.php
    Quick fix to paragraph format

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
           <img src = "http://www.fns.usda.gov/sites/default/files/wic---250_0.png" />
           <h1>WIC Central</h1>
           <p>Welcome! The National WIC Association runs this website and works with WIC agencies to give you the 
-          most up to date info in one spot. You can trust this website because we are the WIC community.</p>  
+          most up to date info in one spot. </br>You can trust this website because we are the WIC community.</p>  
        </div>
        </div>
     </div><!--This extra div closes an open div from the menubar.php and must be included after the content class -->
