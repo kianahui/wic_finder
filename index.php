0ee34c69d1da0ac500663e1bba56a98bed4183bf
kianahui <kianah@stanford.edu>
2013-11-11 13:56:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,17 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css"/>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script type="text/javascript" src="jquery.touchSwipe.js"></script>
-    <script type="text/javascript">
-      $(window).load(function(){
-        $("[data-toggle]").click(function() {
-          var toggle_el = $(this).data("toggle");
-          $(toggle_el).toggleClass("open-sidebar");
-        });
-         $(".swipe-area").swipe({
-              swipeStatus:function(event, phase, direction, distance, duration, fingers)
-                  {
-                      if (phase=="move" && direction =="right") {
-                           $(".container").addClass("open-sidebar");
-                           return false;
-                      }
-                      if (phase=="move" && direction =="left") {
-                           $(".container").removeClass("open-sidebar");
-                           return false;
-                      }
-                  }
-          }); 
-      });
-      
-    </script>
-</head>
-<body>
-    <div class="container">
-       <?php
-        include'menubar.php';
-       ?>
-       <div class="content">
-          <img src = "http://www.fns.usda.gov/sites/default/files/wic---250_0.png" />
-          <h1>WIC Central</h1>
-          <p>Welcome! The National WIC Association runs this website and works with WIC agencies to give you the 
-          most up to date info in one spot. </br>You can trust this website because we are the WIC community.</p>  
-       </div>
-       </div>
-    </div><!--This extra div closes an open div from the menubar.php and must be included after the content class -->
-</body>
-</html>
+<?php
+include_once('header.php');
+?>
+
+<img src="http://www.fns.usda.gov/sites/default/files/wic---250_0.png" alt="">
+<h1>WIC Central</h1>
+<p>
+	Welcome! The National WIC Association runs this website and works with WIC agencies to give you the 
+	most up to date info in one spot. </br>
+	You can trust this website because we are the WIC community.
+</p>  
+
+<?php
+include_once('footer.php');
+?>
+
 
