50233868b38b0d358318b85c5ea05518cd62f8f8
lilysierra1 <lilysierra1@gmail.com>
2013-11-13 13:56:27 -0800

    Create doLookup.php

---
+++ mobileFolder/doLookup.php
@@ -0,0 +1,27 @@
+<?php
+// doLookup.php: maps State in $_GET['state'] to agencies
+
+$State = $_GET['state'];
+$con = mysqli_connect("localhost", "root");
+mysqli_select_db($con, "mydb");
+
+$sql="SELECT Agency FROM foodList WHERE State = '".$State."';"; 
+$result=mysqli_query($con, $sql); 
+
+echo "<select name=\"agency\" id =\"agencydropdown\">";
+echo "<option value=\"\">Please select your agency</option>";
+
+while ($row=mysqli_fetch_array($result)) { 
+	$Agency=$row["Agency"]; 
+
+	if ($Agency !="") {
+
+		$option="<OPTION VALUE=\"$Agency\">$Agency</option>";
+		echo $option;
+	}
+} 
+
+echo "</select>";
+
+mysqli_close($con);
+?>
