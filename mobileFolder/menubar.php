382153f713a646cf4f9b169ec47f837905cfd5b9
lilysierra1 <lilysierra1@gmail.com>
2013-11-13 13:58:36 -0800

    Create menubar.php

---
+++ mobileFolder/menubar.php
@@ -0,0 +1,28 @@
+<!--This code is for the menu bar on the right that will display the logo and the 5 user options-->
+<div id="sidebar">
+    <ul>
+        <li><a href= "index.php"><img src = "WIC_logo_white.png" width = "150px"/></a></li>
+        <li><a href="location.php">Find a clinic</a></li>
+        <li><a href="foodlist.php">See Food List</a></li>
+     </ul>
+     <div id="footer"> <p>
+    <div class="social">
+    <a href="http://www.facebook.com/nationalwicassociation" target="_blank"><img src ="recycle.png"/></a>
+    </div>
+    
+    &copy; 2013 National WIC Association<br>
+    2001 S Street, NW Suite 580<br>
+    Washington, DC 20009<br>
+    Voice: 202.232.5492<br>
+    Fax: 202.387.5281
+</p>
+</div>
+</div>
+<div class="main-content">
+	<a href="#" data-toggle=".container" id="sidebar-toggle">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
+
+<!--This div will be closed in the file where it is included-->
