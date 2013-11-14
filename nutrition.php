e911b8b087c26fba6b49a34c54cd199c14c5dd68
kianahui <kianah@stanford.edu>
2013-11-14 00:57:02 -0800

    Create nutrition.php

---
+++ nutrition.php
@@ -0,0 +1,39 @@
+<?php
+include_once('header.php');
+?>
+
+<h1>
+	Nutrition and Healthy Eating Tips
+</h1>
+
+<h2>
+	Selecting Whole Grain Foods
+</h2>
+<p>Look for food with a whole grain listed as the first ingredient. A food has more of the first ingredient 
+	than any other ingredient.</p>
+
+	<h2>Here are a few ways to eat more fruits and vegetables every day!</h2>
+
+	<ul>
+
+		<li>Keep cut up fruits and vegetables ready for a quick salad.</li>
+		<br>
+		<li>Cut up raw vegetables like carrots, celery, jicama, cucumber, peppers, and zucchini and store in the 
+			refrigerator for an easy snack. </li>
+			<br>
+			<li>Add a colorful variety of vegetables to soups, casseroles, stews, rice dishes and spaghetti sauces.</li>
+			<br>
+			<li>Add raw, sauteed, or roasted vegetables to scrambled eggs, quesadillas, burritos, sandwiches, or 
+				wraps. </li>
+				<br>
+				<li>Keep fresh fruit in easy reach for a quick snack. </li>
+				<br>
+				<li>Use sliced or chopped fruit as a topping for pancakes, waffles, and cereal.</li>
+				<br>
+				<li>Add chopped or mashed fruit or berries to muffins, cake, and cookies. </li>
+
+	</ul>
+
+<?php
+include_once('footer.php');
+?>
