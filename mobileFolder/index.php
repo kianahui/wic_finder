e25f4585c177916d2972904ca32d7592b722b7f8
lilysierra1 <lilysierra1@gmail.com>
2013-11-15 14:41:58 -0800

    update from after class

--- mobileFolder/index.php
+++ mobileFolder/index.php
@@ -5,8 +5,18 @@ include_once('header2.php');
           <img src = "http://www.fns.usda.gov/sites/default/files/wic---250_0.png" width="450px"/>
           <h1>WIC Central</h1>
           <div width = "50">
-            <p style ="resize: none";> Welcome! The National WIC Association runs this website and works with WIC agencies to give </br>you the 
-          most up to date info in one spot. You can trust this website because we are the WIC community.</p>  
+           <p>
+          WIC safeguards the health of low-income women, infants, and children up to age 5 who are at 
+          nutrition risk by providing nutritious foods to supplement diets, information on healthy eating, 
+          and referrals to health care. 
+          </p>
+
+          <p> 
+        This website was created by the National WIC Association (NWA) 
+        which is the non-profit arm and advocacy voice of the WIC Program. 
+        Our association members are WIC agencies, and we work with them to keep the information on this website up to date.
+        <br>
+        </p> 
        </div>
        </div>
        </div>
