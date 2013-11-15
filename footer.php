f828ed3354f5945cbc06e64d807e5594145c42c9
kianahui <kianah@stanford.edu>
2013-11-15 14:22:19 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
+		</div>
+
+	<div id="footer">
+	<!-- 	<div class="social">
+		<a href="http://www.facebook.com/nationalwicassociation" target="_blank"><img src ="recycle.png"/></a>
+	</div> -->
+	<p>
+		&copy; 2013 National WIC Association
+	</p>
 	</div>
 </div><!--This extra div closes an open div from the menubar.php and must be included after the content class -->
 
+
 </body>
-<div id="footer">
-<!-- 	<div class="social">
-	<a href="http://www.facebook.com/nationalwicassociation" target="_blank"><img src ="recycle.png"/></a>
-	</div> -->
-	<p>
-	&copy; 2013 National WIC Association<br>
-</p>
-</div>
 </html>
