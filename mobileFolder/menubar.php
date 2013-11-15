cbd99084f468304159b915fc9d70e2ae6a6e8fae
lilysierra1 <lilysierra1@gmail.com>
2013-11-15 14:43:51 -0800

    update from after class

--- mobileFolder/menubar.php
+++ mobileFolder/menubar.php
@@ -1,9 +1,11 @@
 <!--This code is for the menu bar on the right that will display the logo and the 5 user options-->
 <div id="sidebar">
     <ul>
-        <li><a href= "index.php"><img src="http://i.imgur.com/0kdbIIK.png" width = "150px" title="Hosted by imgur.com" /></a></li>
+        <li><a href= "index.php"><img src="http://i.imgur.com/0kdbIIK.png" width = "150px" title="Hosted by imgur.com" id="picture"/></a></li>
         <li><a href="location.php">Find a clinic</a></li>
         <li><a href="foodlist.php">See Food List</a></li>
+        <li><a href="nutrition.php">Nutrition</a></li>
+        <li><a href="breastfeeding.php">Breastfeeding</a></li>
      </ul>
      <div id="footer"> <p>
     <div class="social">
