a42b9dff19eb150e53d82f9626dfdb864ec41515
Samuel Finlayson <samfin55@gmail.com>
2013-11-18 02:13:46 -0500

    config

--- codeigniter/application/config/config.php
+++ codeigniter/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://http://54.200.248.73/wic_finder/';
+$config['base_url']	= 'http://http://54.200.248.73/wic_finder/codeigniter/index.php/';
 
 /*
 |--------------------------------------------------------------------------
