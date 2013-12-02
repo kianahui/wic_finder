d2ac569a0900900fea5a477d4db59020a69967ee
lilysierra1 <lilysierra1@gmail.com>
2013-12-01 22:22:00 -0800

    Create footer.php

---
+++ mobileFolder/footer.php
@@ -0,0 +1,12 @@
+		</div>
+
+	<div id="footer">
+	<p>
+		&copy; 2013 National WIC Association
+	</p>
+	</div>
+</div><!--This extra div closes an open div from the menubar.php and must be included after the content class -->
+
+
+</body>
+</html>
