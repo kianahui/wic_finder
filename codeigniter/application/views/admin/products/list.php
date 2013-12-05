7d5340a81386518c9f667f573d787d76f6e5082a
Samuel Finlayson <samfin55@gmail.com>
2013-12-04 16:53:47 -0800

    page titles

--- codeigniter/application/views/admin/products/list.php
+++ codeigniter/application/views/admin/products/list.php
@@ -15,7 +15,7 @@
       <div class="page-header users-header">
         <h2>
           <?php
-          if ($this->uri->segment(2) == 'products') {echo "Clinics";}
+          if ($this->uri->segment(2) == 'products') {echo 'Clinics';}
           else {echo "Food Lists";}
             ?>
           }
