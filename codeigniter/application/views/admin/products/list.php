9b0b06757afd65a8ef77316f430b5139770269ca
Samuel Finlayson <samfin55@gmail.com>
2013-12-04 16:46:58 -0800

    page titles

--- codeigniter/application/views/admin/products/list.php
+++ codeigniter/application/views/admin/products/list.php
@@ -8,17 +8,13 @@
           <span class="divider">/</span>
         </li>
         <li class="active">
-          <?php echo 
-            if($this->uri->segment(2)=='products'){"Clinics"}else{"Food Lists"}
-            ;?>
+          <?php echo ucfirst($this->uri->segment(2));?>
         </li>
       </ul>
 
       <div class="page-header users-header">
         <h2>
-          <?php echo 
-            if($this->uri->segment(2)=='products'){"Clinics"}else{"Food Lists"}
-            ;?>
+          <?php echo ucfirst($this->uri->segment(2));?> 
           <a  href="<?php echo site_url("admin").'/'.$this->uri->segment(2); ?>/add" class="btn btn-success">Add a new</a>
         </h2>
       </div>
